b1a49f89d50c44e3e8f4feca06f4ca2d5a4ba2d3
MarcoSchipani <mschipani75@gmail.com>
2026-01-23 15:55:41 +0100

    Time: 398 ms (44.28%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,1 +1,10 @@
 # Write your MySQL query statement below
+SELECT 
+    P.firstName,
+    P.lastName, 
+    A.city,
+    A.state
+FROM Person AS P
+LEFT JOIN  Address AS A
+    ON P.personId = A.personId AND
+    A.addressId IS NOT NULL;
