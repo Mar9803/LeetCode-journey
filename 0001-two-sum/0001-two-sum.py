c27563c95bdb292c48e98babcbff402c16d1ed7d
MarcoSchipani <mschipani75@gmail.com>
2026-07-07 13:27:30 +0200

    Time: 0 ms (100%), Space: 20.6 MB (17.74%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        # our index of the db, Struttura: { valore_del_numero: suo_indice }
+        db_index = {}
+        
+        # Facciamo una singola scansione della tabella (Single Table Scan)
+        for i, num in enumerate(nums):
+            # Calcoliamo la riga "complementare" che farebbe scattare la JOIN
+            complemento = target - num
+            
+            # Facciamo una query istantanea O(1) nel nostro indice
+            if complemento in db_index:
+                # Trovato! Facciamo la "JOIN" restituendo l'indice del vecchio 
+                # numero (preso dall'indice) e l'indice di quello corrente (i)
+                return [db_index[complemento], i]
+            
+            # Se non c'è, registriamo il numero corrente nell'indice
+            # così i numeri successivi potranno "fare la query" su di lui
+            db_index[num] = i
+            
+        return [] # Caso limite
