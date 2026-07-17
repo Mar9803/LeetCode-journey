0fe44d897d36278f9a18a8c0762b13162a2e8b60
MarcoSchipani <mschipani75@gmail.com>
2026-07-17 13:45:33 +0200

    Time: 258 ms (53.77%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,11 +1,10 @@
-WITH split_time AS (
-    SELECT *, 
-           LAG(timestamp) OVER(PARTITION BY machine_id, process_id ORDER BY timestamp) AS previus_time
-    FROM Activity
-)
 SELECT 
-    machine_id,
-    ROUND(AVG(timestamp - previus_time), 3) AS processing_time
-FROM split_time
-WHERE activity_type = 'end'
-GROUP BY machine_id;
+    a1.machine_id,
+    ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM Activity a1
+INNER JOIN Activity a2 
+    ON a1.machine_id = a2.machine_id 
+    AND a1.process_id = a2.process_id
+    AND a1.activity_type = 'start' 
+    AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
