a18909360dcd8a078c95af76f7668bfb5d3c7c70
MarcoSchipani <mschipani75@gmail.com>
2026-07-19 02:38:15 +0200

    first exercise distionary, hackerRank

---
+++ 25168-sales-by-match-Hackerrank/25168-sales-by-match.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    pila ={}
+    for i in ar:
+        if i in pila:
+            pila[i]+=1
+        else:
+            pila[i]= 1
+    total_pairs =0 
+    for j in pila.values():
+        total_pairs += j//2
+    return total_pairs
+# nota personale risoluzoneijbijbihgjhv
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
