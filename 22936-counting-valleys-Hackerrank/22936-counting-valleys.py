f02512c7cde4d553a7a6690a11d1eaee0f2446a7
MarcoSchipani <mschipani75@gmail.com>
2026-07-19 11:36:58 +0200

    second exercise completed in hackerRank

---
+++ 22936-counting-valleys-Hackerrank/22936-counting-valleys.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    livello_mare = 0
+    valle_count = 0
+    for i in path:
+        if i == 'U':
+            livello_mare += 1
+            if livello_mare == 0:
+                valle_count += 1
+        elif i == 'D':
+            livello_mare -= 1
+            
+    return valle_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
