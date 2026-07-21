4f12c9f0b40b4d8a7fdbda62eb0c3c41ef1c499c
MarcoSchipani <mschipani75@gmail.com>
2026-07-21 10:59:57 +0200

    sol1 non optimized leetcode 2356

---
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -0,0 +1,7 @@
+Select teacher_id, COUNT(distinct subject_id) as cnt
+FROM Teacher
+GROUP BY teacher_id
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
